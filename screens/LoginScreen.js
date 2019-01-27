34ff411190d90661d5882c865cd69b17a8e5d578
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-26 22:24:47 -0500

    Login Screen working with placeholders, looking into modal for the new users

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,7 +2,27 @@ import React, {Component} from 'react';
 import {StyleSheet, Button, Text, View} from 'react-native';
 
 export default class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: null,
+      user_id: null,
+    };
+
+    this.handleLogin = this.handleLogin.bind(this)
+  }
+
+  handleLogin = () => {
+    console.log('Logging In');
+    this.setState({
+      userName: 'DB_NAME',
+      user_id: 'DB_ID'
+    }, () => console.log('userName', this.state.userName))
+  };
+
   render() {
+    let loggedIn = this.state.userName !== null;
+    console.log(loggedIn);
 
     // Body Content
     return (
@@ -10,15 +30,21 @@ export default class LoginScreen extends Component {
         <Text style={styles.welcome}>Trek Tips</Text>
         <Text style={styles.instructions}>Our goal is for you to have the best possible travel experience.</Text>
         <Text style={styles.instructions}>To get started, please login...</Text>
-        <Button
-          title="Login"
-          color="#FF1589"
-          onPress={() => {
-            this.props.navigation.navigate('Home', {
-              user_id: 23,
-              userName: 'User Name'
-            })
-          }}/>
+        {loggedIn
+          ? <Button
+            title="Continue"
+            color="#FF1589"
+            onPress={() => {
+              this.props.navigation.navigate('Home', {
+                user_id: this.state.user_id,
+                userName: this.state.userName
+              })
+            }}/>
+          : <Button
+            title="Login"
+            color="#FF1589"
+            onPress={this.handleLogin}/>
+        }
       </View>
     )
   }
