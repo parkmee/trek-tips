66ada92690b4036ae83861b69288c1b4aee365ce
MMPark <parkmee@gmail.com>
2019-02-10 19:36:34 -0500

    pointed routes to production server

--- utils/API.js
+++ utils/API.js
@@ -8,61 +8,61 @@ const DEV_SERVER_URL = Platform.OS === 'android'
 
 export default {
   handleLogin: function (accessToken) {
-    return axios.get(`http://${DEV_SERVER_URL}:8000/api/tips/${accessToken}`)
+    return axios.get(`https://trek-tips.herokuapp.com/api/tips/${accessToken}`)
   },
   searchYelp: function(id, location, categories) {
-    return axios.post(`http://${DEV_SERVER_URL}:8000/api/user/${id}/recommendations/`, {location, categories})
+    return axios.post(`https://trek-tips.herokuapp.com/api/user/${id}/recommendations/`, {location, categories})
   },
   // get all users
   getAllUsers: function() {
-    return axios.get(`http://${DEV_SERVER_URL}:8000/api/user`)
+    return axios.get(`https://trek-tips.herokuapp.com/api/user`)
   },
   // create new user
   createUser: function() {
-    return axios.post(`http://${DEV_SERVER_URL}:8000/api/user/new`)
+    return axios.post(`https://trek-tips.herokuapp.com/api/user/new`)
   },
   // find user by id and populate with preferences
   findUserById: function(id) {
-    return axios.get(`http://${DEV_SERVER_URL}:8000/api/user/${id}`)
+    return axios.get(`https://trek-tips.herokuapp.com/api/user/${id}`)
   },
   // add user preference
   addUserPreference: function(id, categoryid) {
-    return axios.post(`http://${DEV_SERVER_URL}:8000/api/user/${id}/category/${categoryid}`)
+    return axios.post(`https://trek-tips.herokuapp.com/api/user/${id}/category/${categoryid}`)
   },
   // delete user preference
   removeUserPreference: function(id, categoryid) {
-    return axios.delete(`http://${DEV_SERVER_URL}:8000/api/user/${id}/category/${categoryid}`)
+    return axios.delete(`https://trek-tips.herokuapp.com/api/user/${id}/category/${categoryid}`)
   },
   // get saved places and populate with place data
   getUserSavedPlaces: function(id) {
-    return axios.get(`http://${DEV_SERVER_URL}:8000/api/user/${id}/places/saved`)
+    return axios.get(`https://trek-tips.herokuapp.com/api/user/${id}/places/saved`)
   },
   // save a place to a user's record (and add it to the places collection if new)
   addUserSavedPlace: function(id, placeObject) {
-    return axios.get(`http://${DEV_SERVER_URL}:8000/api/user/${id}/places/saved/`, { placeObject })
+    return axios.get(`https://trek-tips.herokuapp.com/api/user/${id}/places/saved/`, { placeObject })
   },
   // remove saved place
   removeUserSavedPlace: function(id, placeid) {
-    return axios.get(`http://${DEV_SERVER_URL}:8000/api/user/${id}/places/saved/${placeid}`)
+    return axios.get(`https://trek-tips.herokuapp.com/api/user/${id}/places/saved/${placeid}`)
   },
   // get visited places
   getUserVisitedPlaces: function(id) {
-    return axios.get(`http://${DEV_SERVER_URL}:8000/api/user/${id}/places/visited`)
+    return axios.get(`https://trek-tips.herokuapp.com/api/user/${id}/places/visited`)
   },
   // mark place visited in user's record (and add it to the places collection if new)
   addUserVisitedPlace: function(id, placeObject) {
-    return axios.get(`http://${DEV_SERVER_URL}:8000/api/user/${id}/places/visited/`, { placeObject })
+    return axios.get(`https://trek-tips.herokuapp.com/api/user/${id}/places/visited/`, { placeObject })
   },
   // remove visited place
   removeUserVisitedPlace: function(id, placeid) {
-    return axios.get(`http://${DEV_SERVER_URL}:8000/api/user/${id}/places/visited/${placeid}`)
+    return axios.get(`https://trek-tips.herokuapp.com/api/user/${id}/places/visited/${placeid}`)
   },
   // delete place from place collection -- for cleaning collection during testing
   deletePlace: function(id) {
-    return axios.put(`http://${DEV_SERVER_URL}:8000/api/place/${id}`)
+    return axios.put(`https://trek-tips.herokuapp.com/api/place/${id}`)
   },
   getAllUserPlaces: function(id) {
-    return axios.get(`http://${DEV_SERVER_URL}:8000/api/user/${id}/places`)
+    return axios.get(`https://trek-tips.herokuapp.com/api/user/${id}/places`)
   },
   // get parent categories (yelp categories with no parent aliases defined)
   getParentCategories: function() {
